c3021fa41e1ecb40a8d77aea6edf80ce4c1bf0ce
Vadym Markov <markov.vadym@gmail.com>
2017-01-08 18:38:22 +0100

    Implement WIT API

--- Sources/App/Wit/WitClient.swift
+++ Sources/App/Wit/WitClient.swift
@@ -1,5 +1,51 @@
-struct WitConfiguration {
-  var token = ""
+import Vapor
+import HTTP
+
+struct WitConfig {
+  var token = "L6YMXZKZJRRB7BBBFYJE7CNTNKGQEDLS"
   var version = "20160526"
   var sessionId = ""
+}
+
+protocol WitClient {
+  var drop: Droplet { get }
+  var config: WitConfig { get }
+}
+
+extension WitClient {
+
+  var baseUrl: String {
+    return "https://api.wit.ai/"
+  }
+
+  var headers: [HeaderKey: String] {
+    return [
+      "Authorization" : "Bearer \(config.token)",
+      "Accept" : "application/json",
+      "Content-Type" : "application/json"
+    ]
+  }
+
+  var query: [String: CustomStringConvertible] {
+    return [
+      "v": config.version,
+      "session_id": config.sessionId
+    ]
+  }
+
+  func request(_ method: Method,
+               path: String,
+               query: [String: CustomStringConvertible] = [:],
+               context: Node) throws -> Response {
+    var requestQuery = self.query
+
+    query.forEach { key, value in
+      requestQuery[key] = value
+    }
+
+    let uri = "\(baseUrl)\(path)"
+    let json = try JSON(node: context)
+    let body = try Body.data(json.makeBytes())
+    return try drop.client.request(method, uri, headers: headers, query: requestQuery, body: body)
+  }
 }
